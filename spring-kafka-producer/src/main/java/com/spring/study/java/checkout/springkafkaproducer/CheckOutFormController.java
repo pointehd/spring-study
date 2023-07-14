14c32844504e0a0e9d4cde39ec93fdc4e0703e07
Donghyun <pointehd@naver.com>
2023-07-14 19:10:33 +0900

    Feat: kafka(패캠강의 진행중)

---
+++ spring-kafka-producer/src/main/java/com/spring/study/java/checkout/springkafkaproducer/CheckOutFormController.java
@@ -0,0 +1,17 @@
+package com.spring.study.java.checkout.springkafkaproducer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class CheckOutFormController {
+
+    @GetMapping("/checkOutForm")
+    public String checkOutForm() {
+        log.info("checkOutForm........");
+        return "checkOutForm";
+    }
+
+}
